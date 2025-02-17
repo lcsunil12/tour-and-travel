d138d59c1c9645f0e4175daa36e5c5aa3496d6b5
Sunil Lamichhane <sunillamichhane901@gmail.com>
2025-02-17 08:40:07 +0545

    add dynamic image in tour page card

--- src/pages/tour.page.jsx
+++ src/pages/tour.page.jsx
@@ -1,25 +1,16 @@
 import React from "react";
+import { products } from "../libs/product";
 
-const Details = [
-  {
-    id: 1,
-    name: "Tokyo",
-    slug: "Tokyo City",
-    rating: 5,
-    image: "Tokyo.jpg",
-    description:
-      "Experience unparalleled comfort and precision with our ergonomic mouse pad...",
-    image: "/photo2.jpg",
-  },
-];
+
+
 const TourDetails = () => {
   return (
     <div>
     <div class="bg-gray-100 p-6 border border-gray-300 rounded-lg shadow-md h-56 w-44">
-   {Details.map((item, i) => {
+   {products.map((item, i) => {
     return <div key="id " className="text-white  ">
-        {/* <img  className=
-         "absolute h-44 w-44" src={item.image}/> */}
+         <img  className=
+         "absolute h-44 w-44" src={`/${item.photo}`}/> 
        <h2 className=" text-black  ">{item.name}</h2> 
        <p className=" text-black ">{item.rating}</p>
     
